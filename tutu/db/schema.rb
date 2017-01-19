5de8c91d0749aadfc99979236eb66e704cb46b6f
Alexander Petryakov <shurik_na@bk.ru>
2017-01-19 22:19:49 +0300

    Lesson 13

--- tutu/db/schema.rb
+++ tutu/db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115113648) do
+ActiveRecord::Schema.define(version: 20170119190622) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "railway_stations_routes", force: :cascade do |t|
+    t.integer "railway_station_id"
+    t.integer "route_id"
   end
 
   create_table "routes", force: :cascade do |t|
@@ -24,8 +29,34 @@ ActiveRecord::Schema.define(version: 20170115113648) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+    t.integer  "train_id"
+    t.integer  "departure_station_id"
+    t.integer  "arrival_station_id"
+    t.integer  "user_id"
+    t.index ["arrival_station_id"], name: "index_tickets_on_arrival_station_id"
+    t.index ["departure_station_id"], name: "index_tickets_on_departure_station_id"
+    t.index ["train_id"], name: "index_tickets_on_train_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
+  end
+
   create_table "trains", force: :cascade do |t|
     t.string   "number"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.integer  "railway_station_id"
+    t.integer  "current_station_id"
+    t.integer  "route_id"
+    t.index ["current_station_id"], name: "index_trains_on_current_station_id"
+    t.index ["railway_station_id"], name: "index_trains_on_railway_station_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
